8b484412b0e203677795dae66bcacbff347739fc
mickshen <mickshen7558@gmail.com>
2020-11-25 23:32:25 -0500

    easy install

--- Dirs.py
+++ Dirs.py
@@ -8,23 +8,23 @@ Created on Mon Jul  1 10:29:20 2019
 
 import os
 
-Disk_F = '/media/rain/Win10_F/'
+Disk_F = '/data/mydata/'
 # Disk_F = 'F:\\'
 
-strBaseDir = os.path.join(Disk_F,'KITTI_odometry')
+strBaseDir = os.path.join(Disk_F,'kitti_velodyne')
 str3DFeatNetDir = os.path.join(Disk_F,'KITTI_odometry', 'output_3DFeatNet');
 strUsipBaseDir =   os.path.join(Disk_F, 'USIP')
 
 
-strGroundTruthPosesDir = os.path.join(strBaseDir, 'poses')
+strGroundTruthPosesDir = os.path.join(strBaseDir, 'data_odometry_poses')
 strEstimatedPosesDir = os.path.join(strBaseDir, 'poses_')
 strDejumpyedPosesDir = os.path.join(strBaseDir, 'poses__')
 strRefinedPosesDir = os.path.join(strBaseDir, 'poses___')
 #strRefinedPosesDir = os.path.join(strBaseDir, 'poses___WithoutBackwardUpdate')
 strClosedPosesDir = os.path.join(strBaseDir, 'poses____')
 
-strDataBaseDir = os.path.join(strBaseDir, 'velodyne', 'sequences')
-strCalibDataDir = os.path.join(strBaseDir, 'calib')
+strDataBaseDir = os.path.join(strBaseDir, 'data_odometry_velodyne', 'dataset' ,'sequences')
+strCalibDataDir = os.path.join(strBaseDir, 'data_odometry_calib')
 
 strRespondNetModelPath = './TrainedModels/SphericalRingPCRespondLayer.h5'
 strVoxelPatchEncoderPath = './TrainedModels/EncoderModel4VoxelPatch.h5'
